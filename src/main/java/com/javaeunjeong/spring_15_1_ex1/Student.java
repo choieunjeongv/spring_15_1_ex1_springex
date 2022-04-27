0dd4f866a570bcd9a5d274b97ec1fb5970f85085
choieunheong <mjchoi17@hanmail.net>
2022-04-27 14:57:29 +0900

    validator test

---
+++ src/main/java/com/javaeunjeong/spring_15_1_ex1/Student.java
@@ -0,0 +1,22 @@
+package com.javaeunjeong.spring_15_1_ex1;
+
+public class Student {
+
+	private String id;
+	private int pw;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getPw() {
+		return pw;
+	}
+	public void setPw(int pw) {
+		this.pw = pw;
+	}
+	
+	
+}
